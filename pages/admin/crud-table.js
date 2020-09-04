06e1b554d85fc325ab570321eb5659ebd8fe90b5
Daniel Marchi <cto@ada.solutions>
2020-09-04 09:16:03 +0300

    hotfix

--- pages/admin/crud-table.js
+++ pages/admin/crud-table.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import MaterialTable from "material-table";
 
 function CrudTable() {
-    return (
-        <div>
-            crud table
-        </div>
-    )
+  return (
+    <div>
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          columns={[
+            { title: "Adı", field: "name" },
+            { title: "Soyadı", field: "surname" },
+            { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+            {
+              title: "Doğum Yeri",
+              field: "birthCity",
+              lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+            },
+          ]}
+          data={[
+            {
+              name: "Mehmet",
+              surname: "Baran",
+              birthYear: 1987,
+              birthCity: 63,
+            },
+          ]}
+          title="Demo Title"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default CrudTable
+export default CrudTable;
--- pages/api/check.js
+++ pages/api/check.js
@@ -1,29 +1,31 @@
-import dbConnect from "../../lib/dbConnect";
-import Lead from "../../models/Lead";
+  import dbConnect from "../../lib/dbConnect";
+  import Lead from "../../models/Lead";
 
-export default async function handler(req, res) {
-  const { method, body } = req;
-  await dbConnect();
-  switch (method) {
-    case "POST":
-      const isExistes = await Lead.findOne({ phone: body.phone });
-      if (isExistes === null) {
-        res.status(200).json({ isExistes, body });
-      }
-      res.status(400).json({ success: false, body });
-      break;
-    // case "POST":
-    //   try {
-    //     const { name, phone } = req.body;
-    //     const newLead = new Lead({ name, phone });
-    //     newLead.Save();
-    //     res.status(200).json({ newLead });
-    //   } catch (error) {
-    //     res.status(500).json({ error, message: "something went worng" });
-    //   }
-    //   break;
-    default:
-      res.status(400).json({ success: false, body });
-      break;
+  export default async function handler(req, res) {
+    const { method, body } = req;
+    await dbConnect();
+    switch (method) {
+      case "POST":
+        const isExistes = await Lead.findOne({ phone: body.phone });
+        if (isExistes === null) {
+          return res.status(200).json({ isExistes, body });
+        }
+        else {
+          res.status(400).json({ success: false, body });
+        }
+        break;
+      // case "POST":
+      //   try {
+      //     const { name, phone } = req.body;
+      //     const newLead = new Lead({ name, phone });
+      //     newLead.Save();
+      //     res.status(200).json({ newLead });
+      //   } catch (error) {
+      //     res.status(500).json({ error, message: "something went worng" });
+      //   }
+      //   break;
+      default:
+        res.status(400).json({ success: false, body });
+        break;
+    }
   }
-}
