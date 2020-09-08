dac613e3fc207561ee1e4c1575a4cd55783332b2
Daniel Marchi <cto@ada.solutions>
2020-09-08 13:31:29 +0300

    Logbugfix

--- pages/_document.js
+++ pages/_document.js
@@ -19,10 +19,6 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
           <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=opensanshebrewcondensed:300,400,500,700&display=swap"
-          />
-          <link
             href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
